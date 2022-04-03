dbdac1f378506ffee74441edbf9d326014006f35
Serpa <joao.serpapinto@gmail.com>
2022-04-03 21:23:23 +0100

    fix: add version constraints

--- base.tf
+++ base.tf
@@ -1,6 +1,6 @@
 module "base" {
   source  = "app.terraform.io/curioso-impertinente/base/azure"
-  version = "0.3.2"
+  version = ">= 0.1.0, < 1.0.0"
 
   name = "curimp"
   location = "francecentral"
