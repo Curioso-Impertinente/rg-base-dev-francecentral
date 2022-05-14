fae5e1af0021db5a3d0e7ef7592fddec7023c889
Serpa <joao.serpapinto@gmail.com>
2022-05-14 14:19:44 +0100

    fix: remove deprecated vars from base module

--- base.tf
+++ base.tf
@@ -2,10 +2,8 @@ module "base" {
   source  = "app.terraform.io/curioso-impertinente/base/azure"
   version = ">= 0.1.0, < 1.0.0"
 
-  name = "curimp"
   location = "francecentral"
   environment = "dev"
   address_space = ["10.10.0.0/16"]
   dns_servers = ["168.63.129.16"]
-  subnets = ["vpn", "k8s"]
 }
